9035c1e2929c7a6f0e99735267cdae82e0d9e790
annia valdes <millerl.mb79@gmail.com>
2020-10-31 09:17:04 -0500

    create de server.js

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+//const routes = require('./controllers');
+const path = require('path');
+//const helpers = require('./utils/helpers');
+const app = express();
+const PORT = process.env.PORT || 3001;
+const sequelize = require('./config/connection');
+//const exphbs = require('express-handlebars');
+//const hbs = exphbs.create({ helpers });
+//const session = require('express-session');
+//const SequelizeStore = require('connect-session-sequelize')(session.Store)
+
+// session and connect session to Squelize db
+// const sess = {
+//     secret: 'Super secret secret',
+//     cookie: {},
+//     resave: false,
+//     saveUninitialized: true,
+//     store: new SequelizeStore({
+//         db: sequelize
+//     })
+// };
+
+
+//app.use(session(sess));
+
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.static(path.join(__dirname, 'public')));
+//app.engine('handlebars', hbs.engine);
+//app.set('view engine', 'handlebars');
+//app.use(routes);
+
+// connection with server
+sequelize.sync({ force: true }).then(() => {
+    app.listen(PORT, () => console.log(`Now listening ${PORT}`))
+});
