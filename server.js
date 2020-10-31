a4f2a519fc29cc7c17aa67d6d5407be932bba3f2
annia valdes <millerl.mb79@gmail.com>
2020-10-31 10:42:38 -0500

    create the others routes in controllers home and dashboard

--- server.js
+++ server.js
@@ -5,30 +5,30 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3001;
 const sequelize = require('./config/connection');
-//const exphbs = require('express-handlebars');
-//const hbs = exphbs.create({ helpers });
-//const session = require('express-session');
-//const SequelizeStore = require('connect-session-sequelize')(session.Store)
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({ helpers });
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store)
 
 //session and connect session to Squelize db
-// const sess = {
-//     secret: 'Super secret secret',
-//     cookie: {},
-//     resave: false,
-//     saveUninitialized: true,
-//     store: new SequelizeStore({
-//         db: sequelize
-//     })
-// };
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
 
 
-// app.use(session(sess));
+app.use(session(sess));
 
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.static(path.join(__dirname, 'public')));
-// app.engine('handlebars', hbs.engine);
-// app.set('view engine', 'handlebars');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 app.use(routes);
 
 // connection with server
