5dfc95b486d05d430357647d62f688bc921bf0ac
annia valdes <millerl.mb79@gmail.com>
2020-11-01 20:48:34 -0600

    create a home-routes for login,signup and render for localhost after can show all the post.now the u ser can loggin and l

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,6 +2,63 @@ const router = require('express').Router();
 const sequelize = require('../config/connection')
 const { Post, User, Comment } = require('../models')
 
+router.get('/', (req, res) => {
+    console.log(req.session);
+    Post.findAll({
+        attributes: [
+            'id',
+            'post_content',
+            'title',
+            'created_at'
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbPostData => {
+            const posts = dbPostData.map(post => post.get({ plain: true }));
+            res.render('homepage', { 
+                posts, 
+                loggedIn: req.session.loggedIn
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        console.log('logging in')
+        return;
+    }
+    
+    res.render('login');
+});
+
+router.get('/signup', (req, res) => {
+	// check session variable...if user is logged in redirect to homepage
+	if (req.session.loggedIn) {
+		res.redirect('/');
+		return;
+	}
+	// otherwise render login page
+	res.render('signup');
+});
+
 
 
 module.exports = router;
